9c1706c97a54980e20753315d5e7c7cb143de336
Vishnu Goyal <43448555+goyal-vi@users.noreply.github.com>
2019-09-02 14:02:47 +0530

    Update callbacks.py

--- app/dashapp1/callbacks.py
+++ app/dashapp1/callbacks.py
@@ -1,11 +1,16 @@
 from datetime import datetime as dt
-
+from dash.dependencies import Input, Output
 import pandas_datareader as pdr
-from dash.dependencies import Input
-from dash.dependencies import Output
 
+# register callbacks will update the output which is the graph interactively based on the input of the the dropdown.
+# Essentially,it will update the graph on update of choice of the value from the dropdown.
 
 def register_callbacks(dashapp):
+    """
+    This function takes the Plotly dash application as input and returns the callback of the updation as output.
+    
+    """
+    # It is the standard callback 
     @dashapp.callback(Output('my-graph', 'figure'), [Input('my-dropdown', 'value')])
     def update_graph(selected_dropdown_value):
         df = pdr.get_data_yahoo(selected_dropdown_value, start=dt(2017, 1, 1), end=dt.now())
